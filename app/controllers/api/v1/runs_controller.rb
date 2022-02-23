a5a9b4283ec7b90a968191302541f4aaceb8ddd1
Hannah Malm <hannahemalm616@gmail.com>
2022-02-22 19:03:44 -0600

    added in run controller

--- app/controllers/api/v1/runs_controller.rb
+++ app/controllers/api/v1/runs_controller.rb
@@ -1,2 +1,31 @@
 class Api::V1::RunsController < ApplicationController
+
+    def index 
+        @runs = Run.all 
+        render json: @runs 
+    end 
+
+    def create 
+        @run = Run.new(run_params)
+        if @run.save 
+            render json: @run
+        else 
+            render json:{error: 'Please try again'}
+        end 
+    end 
+
+    def show 
+        @run = Run.find(params[:id])
+        render json: @run 
+    end 
+
+    def destroy
+        @run = Run.find(params[:id])
+        @run.destroy!
+    end 
+
+    private run_params
+        params.require(:run).permit(:category, :total_distance)
+    end
+
 end
--- app/models/run.rb
+++ app/models/run.rb
@@ -1,4 +1,15 @@
 class Run < ApplicationRecord
     has_many :logs
     validates :category, presence: true
+
+    #add up the total distance based on logs distance
+    #if run id == run_id
+    def update_distance(log)
+        if run.id == run_id 
+            self.distance = self.distance + log.distance 
+            self.save 
+        else 
+            return "Test"
+        end 
+    end 
 end
