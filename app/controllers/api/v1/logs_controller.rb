275bf39964134b8834b4bf15878f7e2aae2a05c6
Hannah Malm <hannahemalm616@gmail.com>
2022-02-22 19:26:33 -0600

    updating logs controllers

--- app/controllers/api/v1/logs_controller.rb
+++ app/controllers/api/v1/logs_controller.rb
@@ -1,6 +1,6 @@
 class Api::V1::LogsController < ApplicationController
 
-    before_action :set_log
+    before_action :find_log
 
     #just want the logs associted with a specific type of run 
     def index 
@@ -11,17 +11,17 @@ class Api::V1::LogsController < ApplicationController
 
     #api/v1/runs/1/logs
     #run_id is within the logs table so you are finding all the logs associated with a run
-    def set_log 
+    def find_log 
         @run = Run.find(params[:run_id])
     end 
 
     def create 
-        @logs = Log.new(log_params)
-        if @logs.save 
-            render json: @logs
-        else 
-            render json:{error: 'Please try again'}
-        end 
+        @log = @run.logs.new(log_params)
+        if @log.save 
+            render json: @log 
+        else
+            render json: {error: 'Please complete required fields'}
+        end    
     end 
 
     def show 
