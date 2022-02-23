9d20cd5f632978ccb55dccb70100c237c5ae08b3
Hannah Malm <hannahemalm616@gmail.com>
2022-02-22 19:15:27 -0600

    updated namespacing in routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,14 @@ Rails.application.routes.draw do
 
   #http://localhost:3000/api/v1
 
+
+  #namespacing so you can only access the logs nested inside of a run type
+  #there is a run Id asscoiated with request
   namespace :api do 
     namespace :v1 do 
-      resources :logs
-      resources :runs
+      resources :runs do
+        resources :logs 
+      end 
     end 
   end 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
